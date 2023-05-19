b88b667a4b758022a899c1a9f5bb649f9f60adeb
DJMendoza3 <DJM3webdev@gmail.com>
2023-05-19 09:39:14 -0500

    Added basic user regestration to site. Still need to add authentication to route.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,24 +1,59 @@
 using Microsoft.AspNetCore.Mvc;
-using TeachersPet.Models;
+using Microsoft.EntityFrameworkCore;
+using TeachersPet.Context;
+using TeachersPet.Entities;
 
 namespace TeachersPet.Controllers;
 
 [ApiController]
-[Route("users")]
+[Route("api")]
 
 public class UserController : Controller
 {
 
     private readonly ILogger<UserController> _logger;
+    private readonly SiteContext _context;
 
-    public UserController(ILogger<UserController> logger)
+    public UserController(ILogger<UserController> logger, SiteContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    [HttpGet]
-    public JsonResult Get()
+    [HttpPost("login")]
+    public JsonResult Login()
     {
         return Json(new User { Id = 1, Name = "John Doe", Email = "test email" });
     }
+
+    [HttpPost("register")]
+    public async Task<ActionResult> Register()
+    {
+        User user = new User { Name = "John", Email = "Email@test.com" };
+        if(await _context.Users.AnyAsync(u => u.Name == user.Name)) {
+            return BadRequest("A user with name " + user.Name + " already exists");
+        }
+        try
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("User created");
+        } 
+        catch 
+        {
+            return BadRequest("Error creating user");
+        }
+    }
+
+    [HttpGet("refresh")]
+    public JsonResult Refresh()
+    {
+        return Json(new User { Id = 1, Name = "John Doe", Email = "test email" });
+    }
+    [HttpGet("logout")]
+    public JsonResult Logout()
+    {
+        return Json(new User { Id = 1, Name = "John Doe", Email = "test email" });
+    }
+
 }
