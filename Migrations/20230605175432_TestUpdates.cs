42b75b327953cf36d5c4b8abcd66a46d2568660e
DJMendoza3 <DJM3webdev@gmail.com>
2023-06-05 12:59:57 -0500

    Updated database with new test entities.

---
+++ Migrations/20230605175432_TestUpdates.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachersPet.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestName",
+                table: "Tests",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestName",
+                table: "Tests");
+        }
+    }
+}
