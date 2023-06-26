7369308afa28d443358d05d6b373cc2828912407
DJMendoza3 <DJM3webdev@gmail.com>
2023-06-26 15:28:37 -0500

    Added test result entity

---
+++ Entities/TestResult.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeachersPet.Entities;
+
+public class TestResult 
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public Test Test { get; set; } = null!;
+    public Student Student { get; set; } = null!;
+    public float Result { get; set; } = 0;
+}
