584ffce6e05f4ba49d3e23a129902e94fe5f75d3
DJMendoza3 <DJM3webdev@gmail.com>
2023-06-26 15:27:44 -0500

    Added note entity

---
+++ Entities/Note.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeachersPet.Entities;
+
+public class Note 
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string prompt { get; set; } = null!;
+}
