65c5fd433b47cca5dbc655a250d806f62a6e3f36
DJMendoza3 <DJM3webdev@gmail.com>
2023-06-26 15:28:54 -0500

    Updated user entity to be abstract class that other specific users inherit from

--- Entities/User.cs
+++ Entities/User.cs
@@ -4,13 +4,11 @@ using System.Text.Json.Serialization;
 
 namespace TeachersPet.Entities;
 
-public class User
+public abstract class User
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    [Required]
-    public int Role_id { get; set; } = 0;
     [Required]
     public string Name { get; set; } = null!;
     [Required]
@@ -18,6 +16,7 @@ public class User
     [Required]
     public string Password { get; set; } = null!;
     [Required]
-    public float Credits { get; set; } = 0;
-    public ICollection<Test> Tests { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public School School { get; set; } = null!;
+
 }
