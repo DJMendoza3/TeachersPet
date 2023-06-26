41683356b47940cd0588e966d2972584492c621b
DJMendoza3 <DJM3webdev@gmail.com>
2023-06-26 15:27:03 -0500

    Added active exam entity

---
+++ Entities/ActiveExam.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeachersPet.Entities;
+
+public class ActiveExam
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public Student Student { get; set; } = null!;
+    public Test Test { get; set; } = null!;
+    public DateTime Start { get; set; }
+    public DateTime TimeLimit { get; set; }
+}
